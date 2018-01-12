524d2e1bf9c787d1fb36c9bfab667ff3f0b927d8
olivierbranch <delalande.o@g.com>
2018-01-12 11:27:10 +0100

    Setup app for herokunode

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
+
+// pour mémoire
+// mlab: 'mongodb://<olivier.d>:<mdp>@ds247357.mlab.com:47357/todo-api'
